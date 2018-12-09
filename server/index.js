0b3e5f24bca9fbc057556cb4036f61feba41cbff
Akira Ryuju Ichimura <akiraryuju@gmail.com>
2018-12-09 14:56:42 +0000

    Added Code Comments

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+//The body of the code - brings everything together
+
+//Imports classes
 var webserver = require('./webserver');
 var robot = require('./robot');
 
-// INIT ROBOT
+// INIT ROBOT - Make instance of robot
 var robot1 = new robot({
   // comPort: "COM3", // TODO: If we ever wanted to instantiate multiple Arduinos on different COM ports
+  //Specifies first finger
   finger1: {
     minAngle: 0,
     maxAngle: 174,
@@ -28,6 +32,7 @@ var ws1 = new webserver({
 });
 
 // ROUTES & BUSINESS LOGIC
+// Handles requests from client
 function routes(request, response, serve) {
   var path = request.uri.path;
 
@@ -47,7 +52,7 @@ function routes(request, response, serve) {
   }
 
   if (path.indexOf("/api/goto?angle=") > -1) { // If "?angle=" exists in the string 'path' then...
-    var angle = path.substr(16);
-    robot1.finger1.goto(angle);
+    var angle = path.substr(16); //takes the characters from the string starting from the 16th characters until the end of the string
+    robot1.finger1.goto(angle); // This needs to be able to handle things if it isnt an integer
   }
 }
--- server/robot.js
+++ server/robot.js
@@ -1,22 +1,26 @@
 const five = require("johnny-five");
 
+//Class so that the whole thing can be used as an object
 class robot {
   constructor(options, callback) {
     //const board = new five.Board(); // TODO: Specify COM port from options
     const board = new five.Board();
 
+    // COnstructs the first finger
     this.finger1 = new digit({
       minAngle: options.finger1.minAngle,
       maxAngle: options.finger1.maxAngle,
       pin: options.finger1.pin
     });
-
+    //Arrow function does magic to 'this' 
+    //// Lexical this - The this will not refer to board but will instead refer to constructor/ robot
     board.on("ready", () => {
       this.finger1.bind();
       callback();
     });
   }
 }
+
 
 class digit {
   constructor(options) {
@@ -32,13 +36,17 @@ class digit {
     this.pin = options.pin;
 
   }
+  //Connects servo to an instance of the finger
+  //Will only run once the board is ready - The methods need the board to be ready first to run
   bind() {
     this.servo = new five.Servo({
       pin: this.pin,
       deviceRange: [0, 180]
     });
   }
+  //Method to do the sweep function
   sweep() {
+    //Will try to execute the code within the try but will do the Catch err part if any error is thrown
     try {
       this.servo.sweep();
       this.log(`Sweeping...`);
@@ -50,7 +58,7 @@ class digit {
     // TODO: add speed argument
     try {
       this.servo.to(this.minAngle);
-      this.log(`Going to angle ${this.minAngle}`);
+      this.log(`Going to angle ${this.minAngle}`); //New way to combine String and Variables in a log
     } catch (err) {
       this.error(err);
     }
@@ -65,7 +73,7 @@ class digit {
     }
   }
   goto(angle, err) {
-    if (angle < this.minAngle || angle > this.maxAngle) {
+    if (angle < this.minAngle || angle > this.maxAngle) { //Makes sure that the requested Angle is within bounds
       err('Angle out of bounds!');
     } else {
       try {
@@ -81,7 +89,7 @@ class digit {
     // TODO: return console message to webserver
   }
   error(err) {
-    this.log(`Digit error. (${err.message})`);
+    this.log(`Digit error. (${err.message})`); //Only throws back a readable, necessary part of the error message
   }
   getInfo() {
     // TODO: return this class' information
@@ -91,4 +99,5 @@ class digit {
   }
 }
 
+//Allows the class robot to be able to be used 
 module.exports = robot;
